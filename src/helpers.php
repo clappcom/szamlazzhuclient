336445fc19e2d75ee3465d85ba8bb98264b29406
Hodász Ágoston <hodasza@gmail.com>
2017-10-21 15:57:12 +0200

    update array_where usage
    Since Laravel 5.3 the array_where should be used like this:
    
    $array = array_where($array, function ($value, $key) {
        return is_string($value);
    });
    
    https://laravel.com/docs/5.3/helpers#method-array-where

--- src/helpers.php
+++ src/helpers.php
@@ -4,10 +4,10 @@ if (!function_exists('sortArrayKeysToOrder')){
     function sortArrayKeysToOrder($array, $keysOrder){
         if (!is_array($array)) return $array;
         $keysOrder = array_flip($keysOrder);
-        $nonExistingItems = array_where($array, function($key, $value) use ($keysOrder){
+        $nonExistingItems = array_where($array, function($value, $key) use ($keysOrder){
             return !isset($keysOrder[$key]);
         });
-        $existingItems = array_where($array, function($key, $value) use ($keysOrder){
+        $existingItems = array_where($array, function($value, $key) use ($keysOrder){
             return isset($keysOrder[$key]);
         });
 
