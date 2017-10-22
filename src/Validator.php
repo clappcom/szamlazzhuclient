e1646909daf05fa9a016f359faab5d287fb122c1
dsge <gerx03@gmail.com>
2017-10-22 21:18:03 +0200

    - specify the exact minimum version on illuminate/validation - fix laravel 5.4 compatibility - update travis.yml to test for php7

--- src/Validator.php
+++ src/Validator.php
@@ -2,12 +2,19 @@
 
 namespace Clapp\SzamlazzhuClient;
 
-use Symfony\Component\Translation\Translator;
+use Symfony\Component\Translation\Translator as SymfonyTranslator;
 use Illuminate\Validation\Validator as BaseValidator;
+use Illuminate\Translation\Translator as IlluminateTranslator;
+use Illuminate\Translation\ArrayLoader;
+
 
 class Validator extends BaseValidator{
 
     public static function make(array $data, array $rules, array $messages = [], array $customAttributes = []){
-        return new static(new Translator('en'), $data, $rules, $messages, $customAttributes);
+        try{
+            return new static(new IlluminateTranslator(new ArrayLoader(), 'en'), $data, $rules, $messages, $customAttributes);
+        }catch(\Exception $e){
+            return new static(new SymfonyTranslator('en'), $data, $rules, $messages, $customAttributes);
+        }
     }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,13 +1,35 @@
 <?php
 
+if (!function_exists('illuminate_array_where')){
+    /**
+     * Note: Copied from Laravel 5.2 to circumvent a breaking change on "array_where" in Laravel 5.3
+     *
+     * Filter the array using the given callback.
+     *
+     * @param  array  $array
+     * @param  callable  $callback
+     * @return array
+     */
+    function illuminate_array_where($array, callable $callback)
+    {
+        $filtered = [];
+        foreach ($array as $key => $value) {
+            if (call_user_func($callback, $key, $value)) {
+                $filtered[$key] = $value;
+            }
+        }
+        return $filtered;
+    }
+}
+
 if (!function_exists('sortArrayKeysToOrder')){
     function sortArrayKeysToOrder($array, $keysOrder){
         if (!is_array($array)) return $array;
         $keysOrder = array_flip($keysOrder);
-        $nonExistingItems = array_where($array, function($key, $value) use ($keysOrder){
+        $nonExistingItems = illuminate_array_where($array, function($key, $value) use ($keysOrder){
             return !isset($keysOrder[$key]);
         });
-        $existingItems = array_where($array, function($key, $value) use ($keysOrder){
+        $existingItems = illuminate_array_where($array, function($key, $value) use ($keysOrder){
             return isset($keysOrder[$key]);
         });
 
@@ -19,3 +41,5 @@ if (!function_exists('sortArrayKeysToOrder')){
         return $array;
     }
 }
+
+
